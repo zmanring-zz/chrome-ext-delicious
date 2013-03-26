094f64f76fbfdb212b0dad0c09dd8ee1dbf16caa
Scott Christopherson <scott@scott-christopherson.com>
2013-03-26 00:22:36 -0400

    Add unit tests for listFilter

--- spec/filters.js
+++ spec/filters.js
@@ -3,6 +3,35 @@
 describe('filters', function() {
   beforeEach(module('yum.filters'));
 
+  describe('list', function() {
+    describe('when array is empty', function() {
+      it('should return an empty string', inject(function(listFilter) {
+        expect(listFilter([])).toEqual('');
+      }));
+    });
+
+    describe('when array contains one item', function() {
+      it('should return correct results', inject(function(listFilter) {
+        expect(listFilter(['foo'])).toEqual('foo');
+      }));
+    });
+
+    describe('when array contains multiple items', function() {
+      it('should return correct results', inject(function(listFilter) {
+        expect(listFilter(['foo', 'bar'])).toEqual('foo, bar');
+        expect(listFilter(['foo', 'bar', 'baz'])).toEqual('foo, bar, baz');
+        expect(listFilter(['foo', 'bar baz'])).toEqual('foo, bar baz');
+      }));
+    });
+
+    describe('when array contains multi-word items', function() {
+      it('should return correct results', inject(function(listFilter) {
+        expect(listFilter(['bar baz'])).toEqual('bar baz');
+        expect(listFilter(['foo', 'bar baz'])).toEqual('foo, bar baz');
+      }));
+    });
+  });
+
   describe('filterByWord', function() {
     var links = [
       {description: 'verde', tags: ['foo', 'bar', 'baz']},
