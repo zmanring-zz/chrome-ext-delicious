38b0a6e659be7926a7011911806e007d80fea83a
zmanring <zmanring@maxmedia.com>
2013-05-16 22:13:55 -0400

    Silly manifest

--- assets/javascripts/background.js
+++ assets/javascripts/background.js
@@ -1,7 +1,6 @@
 
 function injectModal(info, tab) {
   chrome.tabs.insertCSS(null, { file:"/assets/stylesheets/background.css" });
-  // chrome.tabs.executeScript(null, { file:"/vendor/javascripts/jquery-1.9.0.min.js" });
   chrome.tabs.executeScript(null, { file:"/assets/javascripts/modal.js" });
 }
 
